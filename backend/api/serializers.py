1939a77e446a73b7f314480718e794c3eda30793
Аркадий Балакшин <Darwin227@yandex.ru>
2023-11-07 21:28:11 +0300

    foodgram v1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -96,7 +96,8 @@ class ReadRecipesSerializer(serializers.ModelSerializer):
         )
 
 
-class WriteRecipeseSerializer(GetIngredientsMixin, serializers.ModelSerializer):
+class WriteRecipeseSerializer(GetIngredientsMixin, 
+                              serializers.ModelSerializer):
     """Сериализация объектов типа Recipes. Запись рецептов."""
 
     tags = serializers.PrimaryKeyRelatedField(
