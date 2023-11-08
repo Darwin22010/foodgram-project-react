c1b392b9d1ec9bb8895ccc3dfd4d8444acc72b60
Аркадий Балакшин <Darwin227@yandex.ru>
2023-11-08 15:36:29 +0300

    user admin model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import Follow
 
+User = get_user_model()
 
 class CustomUserAdmin(BaseUserAdmin):
-    list_display = ("email", "username")
+    list_display = ("email", "username",)
     list_filter = ("email", "username")
 
+admin.site.register(User, CustomUserAdmin)
 
 class FollowAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "author")
@@ -16,6 +19,5 @@ class FollowAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related('user', 'author')
-
 
 admin.site.register(Follow, FollowAdmin)
