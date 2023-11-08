5b977d11e2ddeb43202d1d1ea85a1db94e0f63a2
Аркадий Балакшин <Darwin227@yandex.ru>
2023-11-08 15:04:46 +0300

    foodgram v2

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
 from django.db import transaction
-from django.db.models import BooleanField, Exists, OuterRef, Sum, Value
+from django.db.models import BooleanField, Sum, Value
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
